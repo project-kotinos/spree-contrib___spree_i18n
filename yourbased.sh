5adca2a8bb2bed7c2f5d629bfb362b085791f98f
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-14 20:23:41 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -13,16 +13,16 @@ export RACK_ENV=test
 export MERB_ENV=test
 export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
 
-apt-get update && apt-get install -y tzdata
+apt-get update && apt-get install -y tzdata unzip
 gem install bundler -v 2.0.1
 # before_install
 # mysql -u root -e "GRANT ALL ON *.* TO 'travis'@'%';"
 wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/share/
-sudo chmod +x /usr/local/share/chromedriver
-sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+mv -f ~/chromedriver /usr/local/share/
+chmod +x /usr/local/share/chromedriver
+ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 # install
 bundle install --jobs=3 --retry=3
 # script
