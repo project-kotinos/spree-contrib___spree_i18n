2a5f30472419b8efb32d922f4da15d6d52f19251
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-14 20:20:47 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -16,7 +16,7 @@ export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
 apt-get update && apt-get install -y tzdata
 gem install bundler -v 2.0.1
 # before_install
-mysql -u root -e "GRANT ALL ON *.* TO 'travis'@'%';"
+# mysql -u root -e "GRANT ALL ON *.* TO 'travis'@'%';"
 wget -N https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip -P ~/
 unzip ~/chromedriver_linux64.zip -d ~/
 rm ~/chromedriver_linux64.zip
